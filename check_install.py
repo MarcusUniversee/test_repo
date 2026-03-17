7e3ebb12860ef9a293b88d3f6c7b5350764df14f
MarcusUniversee <33048697+MarcusUniversee@users.noreply.github.com>
2026-03-17 16:16:19 -0700

    installation of packages

---
+++ check_install.py
@@ -0,0 +1,6 @@
+import requests
+import colorama
+
+print("requests version:", requests.__version__)
+print("colorama version:", colorama.__version__)
+print("Both packages installed successfully!")
